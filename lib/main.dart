bc5c02d78f3d0f6afa93f7b703d93a78fd627dd8
Cleberson <pauluci.cleberson@gmail.com>
2024-01-26 15:37:14 -0300

    Wrapped text widget with more complex widgets to make it better look

--- lib/main.dart
+++ lib/main.dart
@@ -11,5 +11,5 @@ void main() {
   // and MaterialApp wrapped Text widget, creating the widget tree.
   // the const keyword is used for dart performance and it's used a lot
   // (singleton pattern)
-  runApp(const MaterialApp(home: Text('Hello World!;)')));
+  runApp(const MaterialApp(home: Scaffold(body: Center(child: Text('Hello World! ;)')))));
 }

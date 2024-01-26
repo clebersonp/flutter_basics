3dd8091e1380fc878563a9d5db8daea7e6c85bd3
Cleberson <pauluci.cleberson@gmail.com>
2024-01-26 14:59:06 -0300

    Created the first app from scratch, initial state of course

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+// import packages and functions from flutter framework or any other
+// third-party feature
+import 'package:flutter/material.dart';
+
+// method main is the entry point for flutter and it must in the main.dart
+// file as well
+void main() {
+  // runApp is the most important function, because it is responsible for
+  // running and create the widget tree and and attach them to mobile screen
+  // MaterialApp and Text are just flutter UI Widgets from flutter framework
+  // and MaterialApp wrapped Text widget, creating the widget tree.
+  // the const keyword is used for dart performance and it's used a lot
+  // (singleton pattern)
+  runApp(const MaterialApp(home: Text('Hello World!;)')));
+}
